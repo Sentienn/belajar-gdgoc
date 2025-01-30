d7149ef47e16dd77f531d3628750677f749d6622
AmdHill <fdhl.ahmd06@gmail.com>
2025-01-30 13:10:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function addTask(){
+    const input = document.getElementById('tf-input');
+    const inputValue = input.value;
+    const task = document.createElement('li')
+    task.textContent = inputValue;
+    task.classList.add('list-item');
+    const taskContainer = document.createElement.appendChild(task);
+    const deleteButton =document.createElement(button);
+    
+}
